0fb16e72220c277466362c39a0412a721ada12cb
Gregory Jefferis <jefferis@gmail.com>
2017-03-23 12:30:53 +0000

    improved examples

--- R/hello.R
+++ R/hello.R
@@ -6,6 +6,17 @@
 #' @export
 #'
 #' @importFrom igraph igraph_opt igraph_options neighborhood
+#' @examples
+#' # OK
+#' nn=set_igraph_option(set.vs.es=FALSE)
+#' \donttest{
+#' # errors out
+#' nn=set_igraph_option()
+#'
+#' # OK again if igraph is attached
+#' library(igraph)
+#' nn=set_igraph_option()
+#' }
 set_igraph_option <- function(g=igraph::make_ring(10), set.vs.es=TRUE) {
   if(set.vs.es) {
     ov=igraph_opt("return.vs.es")
@@ -14,4 +25,5 @@ set_igraph_option <- function(g=igraph::make_ring(10), set.vs.es=TRUE) {
   n=neighborhood(g, 1, mode='out')
   if(set.vs.es)
     igraph_options(return.vs.es=ov)
+  n
 }
